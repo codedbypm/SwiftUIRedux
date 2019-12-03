fbb16f1c4d0d1666fb23b7614e59452b8599426d
Paolo Moroni <paolo@codedby.pm>
2019-12-03 14:39:51 +0100

    Move some library method into redux package

--- Sources/SwiftUIRedux/Core/Reducer.swift
+++ Sources/SwiftUIRedux/Core/Reducer.swift
@@ -5,6 +5,7 @@
 //  Created by Paolo Moroni on 24/11/2019.
 //
 
+import Combine
 import Foundation
 
 public struct Reducer<State, Mutation> {
@@ -58,6 +59,41 @@ public extension Reducer {
             reducers.forEach {
                 $0(&state, mutation)
             }
+        }
+    }
+
+    func combine<State, Mutation>(
+        _ reducers: [(inout State, Mutation) -> Void]
+    ) -> (inout State, Mutation) -> Void {
+        return { (state, mutation) in
+            reducers.forEach {
+                $0(&state, mutation)
+            }
+        }
+    }
+
+    func combine<Action, State, Mutation>(
+        _ storeControllers: [(Action, State) -> AnyPublisher<Mutation, Never>]
+    ) -> (Action, State) -> AnyPublisher<Mutation, Never> {
+        return { (action, state) in
+            let initialResult = Empty<Mutation, Never>(completeImmediately: false).eraseToAnyPublisher()
+            let publisher = storeControllers.reduce(initialResult) { (result, storeController) -> AnyPublisher<Mutation, Never> in
+                return Publishers.Merge(result, storeController(action, state)).eraseToAnyPublisher()
+            }
+
+            return publisher.eraseToAnyPublisher()
+        }
+    }
+
+    func pullback<LocalState, GlobalState, LocalMutation, GlobalMutation>(
+        _ reducer: @escaping (inout LocalState, LocalMutation) -> Void,
+        _ stateKeyPath: WritableKeyPath<GlobalState, LocalState>,
+        _ mutationKeyPath: KeyPath<GlobalMutation, LocalMutation?>
+    ) -> (inout GlobalState, GlobalMutation) -> Void {
+        return { state, mutation in
+            guard let localMutation = mutation[keyPath: mutationKeyPath] else { return }
+
+            reducer(&state[keyPath: stateKeyPath], localMutation)
         }
     }
 }
