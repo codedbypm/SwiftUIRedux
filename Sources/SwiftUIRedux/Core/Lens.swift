a8686269092da5108403f3cd7be16e70924f38d5
Paolo Moroni <paolo@codedby.pm>
2019-12-11 16:49:36 +0100

    Make both a static method

--- Sources/SwiftUIRedux/Core/Lens.swift
+++ Sources/SwiftUIRedux/Core/Lens.swift
@@ -31,7 +31,7 @@ public extension Lens {
         )
     }
 
-    func both<Whole, PartA, PartB>(
+    static func both<Whole, PartA, PartB>(
         _ lhs: Lens<Whole, PartA>,
         _ rhs: Lens<Whole, PartB>
     ) -> Lens<Whole, (PartA, PartB)> {
