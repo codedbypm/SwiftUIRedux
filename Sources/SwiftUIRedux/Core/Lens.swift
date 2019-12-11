5b6981718c3893809e730e1501461fd557aa902b
Paolo Moroni <paolo@codedby.pm>
2019-12-11 16:22:31 +0100

    Add Lens

---
+++ Sources/SwiftUIRedux/Core/Lens.swift
@@ -0,0 +1,50 @@
+//
+//  File.swift
+//  
+//
+//  Created by Paolo Moroni on 11/12/2019.
+//
+
+import Foundation
+
+public struct Lens<Whole, Part> {
+    public let get: (Whole) -> Part
+    public let set: (inout Whole, Part) -> Void
+
+    public init(
+        get: @escaping (Whole) -> Part,
+        set: @escaping (inout Whole, Part) -> Void
+    ) {
+        self.get = get
+        self.set = set
+    }
+}
+
+public extension Lens {
+
+    static func lens<Whole, Part>(
+        _ keyPath: WritableKeyPath<Whole, Part>
+    ) -> Lens<Whole, Part> {
+        return Lens<Whole, Part>(
+            get: { $0[keyPath: keyPath] },
+            set: { $0[keyPath: keyPath] = $1 }
+        )
+    }
+
+    func Both<Whole, PartA, PartB>(
+        _ lhs: Lens<Whole, PartA>,
+        _ rhs: Lens<Whole, PartB>
+    ) -> Lens<Whole, (PartA, PartB)> {
+
+        return Lens<Whole, (PartA, PartB)>(
+            get: {
+                (lhs.get($0), rhs.get($0))
+            },
+            set: { (whole: inout Whole, tuple) in
+                let (partA, partB) = tuple
+                lhs.set(&whole, partA)
+                rhs.set(&whole, partB)
+            }
+        )
+    }
+}
