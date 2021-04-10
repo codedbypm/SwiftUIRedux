07a19305db86e07ace9601a306c886f14195400d
Paolo Moroni <paolo@codedby.pm>
2021-04-10 11:27:44 +0200

    docs: add documentation for Lens and Prism

--- Sources/SwiftUIRedux/Core/Lens.swift
+++ Sources/SwiftUIRedux/Core/Lens.swift
@@ -7,10 +7,20 @@
 
 import Foundation
 
+/// A `Lens` is a type which encapsulates get and set operations of a `Part` over a `Whole`.
+/// It is typically used with Swift structs.
 public struct Lens<Whole, Part> {
+
+    /// A closure that derives `Part` from `Whole`
     public let get: (Whole) -> Part
+
+    /// A closure that sets `Part` on `Whole`
     public let set: (inout Whole, Part) -> Void
 
+    /// Init a `Lens` with a getter and a setter closures.
+    /// - Parameters:
+    ///   - get: the closure acting as getter
+    ///   - set: the closure acting as setter
     public init(
         get: @escaping (Whole) -> Part,
         set: @escaping (inout Whole, Part) -> Void
@@ -18,22 +28,29 @@ public struct Lens<Whole, Part> {
         self.get = get
         self.set = set
     }
-}
 
-public extension Lens {
-
+    /// Init a `Lens` using a keypath.
+    /// - Parameter keyPath: the keypath indentifiying the `Part`
     init(keyPath: WritableKeyPath<Whole, Part>) {
         self = Lens<Whole, Part>(
             get: { $0[keyPath: keyPath] },
             set: { $0[keyPath: keyPath] = $1 }
         )
     }
+}
 
+public extension Lens {
+
+    /// An operator that combines two `Lense` objects and returning a `Lens` whose `Part` is a tuple of the
+    /// combined `Parts`.
+    /// - Parameters:
+    ///   - lhs: the lens operation on `PartA`
+    ///   - rhs: the lens operation on `PartB`
+    /// - Returns: a `Lens` where `Part = (PartA, PartB)`
     static func both<PartA, PartB>(
         _ lhs: Lens<Whole, PartA>,
         _ rhs: Lens<Whole, PartB>
     ) -> Lens<Whole, Part> where Part == (PartA, PartB) {
-
         return Lens<Whole, Part>(
             get: {
                 (lhs.get($0), rhs.get($0))
--- Sources/SwiftUIRedux/Core/Prism.swift
+++ Sources/SwiftUIRedux/Core/Prism.swift
@@ -7,10 +7,24 @@
 
 import Foundation
 
+/// A `Prism` is a type which encapsulates get and set operations of a `Part` over a `Whole`.
+/// It is typically used with Swift enums with associated values.
 public struct Prism<Whole, Part> {
+
+    /// A closure that derives the associated type `Part` of  a `Whole` case. The closure returns an
+    /// `Optional<Part>` since the `Part` can only be retrieved if `Whole` is in the right case.
+    /// If not, we returns nil.
     public let tryGet: (Whole) -> Part?
+
+    /// A closure that sets `Part` on `Whole`. The closure returns `Whole` by setting the associated value `Part`
+    /// on a target enum case.
     public let inject: (Part) -> Whole
 
+
+    /// Default initializer
+    /// - Parameters:
+    ///   - tryGet: the closure returning the enum case associated value
+    ///   - inject: the closure injecting the enum case associated value
     public init(
         tryGet: @escaping (Whole) -> Part?,
         inject: @escaping (Part) -> Whole
